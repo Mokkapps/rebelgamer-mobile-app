5b88aeebed296433e2d9aae221726ef206253559
Taym95 <haddadi@codeyellow.nl>
2018-05-14 19:18:17 +0200

    Add and link react-native-i18n

--- android/app/src/main/java/de/rebelgamer/RebelGamerRSS/MainApplication.java
+++ android/app/src/main/java/de/rebelgamer/RebelGamerRSS/MainApplication.java
@@ -3,6 +3,7 @@ package de.rebelgamer.RebelGamerRSS;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.toast.RCTToastPackage;
 import com.facebook.react.ReactNativeHost;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(), new VectorIconsPackage(), new RCTToastPackage()
+                    new MainReactPackage(),
+            new RNI18nPackage(), new VectorIconsPackage(), new RCTToastPackage()
             );
         }
 
