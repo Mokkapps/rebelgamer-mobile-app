156c9960cb909049c3b0b890b53c87014578d197
Taym95 <haddadi@codeyellow.nl>
2018-05-14 19:47:34 +0200

    Auto detect user language

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import { Alert, Dimensions, NetInfo, Platform } from 'react-native';
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import Toast from 'react-native-toast-native';
+import { I18nextProvider, translate } from 'react-i18next';
+import { getLanguages } from 'react-native-i18n';
 
 import LatestArticles from './components/LatestArticles';
 import ArticleDetails from './components/ArticleDetails';
 import ArticleSearch from './components/ArticleSearch';
 import About from './components/About';
-import translate from './translate';
 import { REBELGAMER_RED } from './constants';
+import i18n, { t } from '../assets/i18n/i18n';
+
+global.t = t;
+
+getLanguages().then((languages) => {
+  // languages is an array of the users preferred order of IETF language tags.
+  // ['en', 'nl-NL', 'pt-BR'] should be English.
+
+  const languagesWithoutCountry = languages.map(
+    language => language.split('-')[0]
+  );
+  const availableLanguages = ['en', 'nl', 'de', 'fr'];
+
+  const preferredLanguage = languagesWithoutCountry.find(language =>
+    availableLanguages.includes(language)
+  );
+
+  switch (preferredLanguage) {
+    case 'de':
+      i18n.changeLanguage('de');
+      break;
+    default:
+      i18n.changeLanguage('en');
+  }
+});
 
 const AppStack = StackNavigator({
   LatestArticles: { screen: LatestArticles },
@@ -32,6 +58,12 @@ type Props = {};
 type State = {
   probablyHasInternet: boolean | undefined
 };
+
+// That will reload app when language changed so the text will changes real-time
+const ReloadAppOnLanguageChange = translate('common', {
+  bindI18n: 'languageChanged',
+  bindStore: false,
+})(AppStack);
 
 export default class App extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -81,13 +113,15 @@ export default class App extends React.Component<Props, State> {
 
   render() {
     return (
-      <AppStack
-        screenProps={{
-          probablyHasInternet: this.state.probablyHasInternet,
-          showErrorAlert: this.showErrorAlert,
-          showToast: this.showToast
-        }}
-      />
+      <I18nextProvider i18n={i18n}>
+        <ReloadAppOnLanguageChange
+          screenProps={{
+            probablyHasInternet: this.state.probablyHasInternet,
+            showErrorAlert: this.showErrorAlert,
+            showToast: this.showToast
+          }}
+        />
+      </I18nextProvider>
     );
   }
 }
